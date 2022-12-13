0a4a446266899ca3f2522ee92390acfd145f1232
moutosi-raka <raka29077997@gmail.com>
2022-12-13 14:33:43 +0600

    added about section

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import flower from '../../../assests/icons8-flower-doodle-96.png'
 
 const About = () => {
     return (
         <div>
-            
+        <h1 className='text-3xl text-deep-green text-center font-bold my-12'>About Me</h1>
+            <div className="flex flex-col w-11/12 mx-auto lg:flex-row my-12">
+                <div className="grid flex-grow h-32 card bg-light-green rounded-box place-items-center">content</div>
+
+                <div className="divider lg:divider-horizontal"><img src={flower} alt='' /></div>
+                <div className="grid flex-grow h-32 card bg-light-green rounded-box place-items-center">content</div>
+            </div>
+
         </div>
     );
 };
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,11 +4,15 @@ import profile2 from '../../../assests/unnamed.webp'
 
 const Banner = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 w-5/6 mx-auto my-12'>
-            <div className='flex items-center p-16'>
-                <p>How to customize daisyUI? daisyUI components come with many variants necessary for design systems and you won't usually need to customize anything. But you can still customize components in many ways.</p>
+        <div className='grid grid-cols-1 lg:grid-cols-2 w-5/6 mx-auto py-12'>
+            <div className='flex items-center py-0 lg:py-16 justify-center lg:justify-start '>
+              <div>
+                <h1 className='text-5xl text-deep-green font-extrabold'>Hello I'm a<br/>
+                full-stack developer
+                </h1>
+              </div>
             </div>
-            <div>
+            <div className='lg:mt-0 mt-16 flex justify-center lg:justify-start '>
                 <img className='w-1/2 rounded-full ring-primary ring ring-offset-base-100 ring-offset-2' src={profile1} alt='' />
             </div>
         </div>
